c47be4e8086d72435ec5aa92b48ac7ff4c9f760f
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2021-02-05 09:54:36 +0100

    Fixed settings file

--- settings.sample.php
+++ settings.sample.php
@@ -2,5 +2,5 @@
 	$api_url = "https://conradb2b-prod.mirakl.net/api";
 	$api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 	
-	$csvPath = "..\conradOrder.csv";
+	$csvPath = "../conradOrder.csv";
 	$last_execution = file_get_contents('last.txt');
