0725c9a5f4407be1cf7c96710382b1d4a2b1d9b7
ADarkHero <maik.riedlsperger@gmail.com>
2023-07-11 15:58:12 +0200

    Phone support

--- index.php
+++ index.php
@@ -22,12 +22,12 @@ if(!file_exists($csvPath)){
 		  $result = $api->getOrders($request);
 		  
 		  //Download result
-		  downloadAPIResult($result, $api_url, $api_key);
+		  downloadAPIResult($result, $api_url, $api_key, $last_execution);
 
 	} catch (\Exception $e) {
 		// An exception is thrown if the requested object is not found or if an error occurs
-		echo "Exception:<br><br>";
-		var_dump($e);
+		echo "Exception:<br><br> ";
+		//var_dump($e);
 	}
 }
 else{
@@ -38,7 +38,7 @@ else{
 /*
 * If there are new orders, download them as csv file.
 */
-function downloadAPIResult($result, $api_url, $api_key){	
+function downloadAPIResult($result, $api_url, $api_key, $last_execution){	
 	$ordercount = $result->getTotalCount();
 		
 	//Check, if there are new orders
@@ -60,8 +60,14 @@ function downloadAPIResult($result, $api_url, $api_key){
 			
 			$orderId = $orderData["id"];
 			
-			//echo "<pre>". var_dump( $result ) . "</pre>";	
 			
+			echo "<pre>";
+			echo var_dump( $o );
+			echo "</pre>";
+			echo "<br><br><br>";
+			
+			
+
 			//Accept order if it wasn't yet
 			if($orderState == "WAITING_ACCEPTANCE"){	
 				$acceptOrderArray = array();
@@ -69,72 +75,96 @@ function downloadAPIResult($result, $api_url, $api_key){
 				foreach($orderLines->getItems() as $ol){
 					array_push($acceptOrderArray, new AcceptOrderLine(['id' => $ol->getData()["id"], 'accepted' => true]));
 				}	
-				
-				// echo "<pre>".var_dump($acceptOrderArray)."</pre>";
-				
+								
 				$api = new ShopApiClient($api_url, $api_key, null);
 				$request = new AcceptOrderRequest($orderId, $acceptOrderArray);
 				$api->acceptOrder($request);
 				
-				echo $orderId." was accepted successfully. ";
+				echo $orderId." was accepted successfully.";
 			}	
 			//Only write order, if it was accepted
-			else if($orderState == "SHIPPING"){
-				//Get shipping price
-				//We need the shipping price in every line of the csv, or our erp system throws an error
-				$shippingPrice = 0;
-				foreach($orderLines->getItems() as $ol){
-					$sp = $ol->getData()["shipping_price"];
-					if($sp > 0){ $shippingPrice = $sp; }
+			//acceptance_decision_date
+			else if ($orderState == "SHIPPING"){
+				//Only import new orders
+				$diff_minutes = (strtotime($last_execution) - $orderData["acceptance_decision_date"]->getTimestamp()) / 60;
+
+				//Log
+				echo " Last Update: " . $orderData["last_updated_date"]->format("Y-m-d\TH:i:s")." Now: ".date('Y-m-d\TH:i:s', strtotime("now"))." DIFF: ".$diff_minutes. "<br><br>";
+				
+				//Only import new orders
+				if($diff_minutes < 0){
+					//Get shipping price
+					//We need the shipping price in every line of the csv, or our erp system throws an error
+					$shippingPrice = 0;
+					foreach($orderLines->getItems() as $ol){
+						$sp = $ol->getData()["shipping_price"];
+						if($sp > 0){ $shippingPrice = $sp; }
+					}
+					
+					//Cycle through orderlines
+					foreach($orderLines->getItems() as $ol){	
+						/* echo "<pre>";
+						echo var_dump( $ol );
+						echo "</pre>"; */
+						
+						$orderAdditonalFields = $orderData["order_additional_fields"];
+						$orderCustomer = $orderData["customer"]->getData()["billing_address"]->getData();
+						$orderShipping = $orderData["customer"]->getData()["shipping_address"]->getData();		
+						
+						$orderOffer = $ol->getData()["offer"];
+						$orderHistory = $ol->getData()["history"];
+						
+						$phone = '';
+						if(isset($orderCustomer["phone"]) && $orderCustomer["phone"] != ""){
+							$phone = $orderCustomer["phone"];
+						}
+						else if(isset($orderCustomer["phone_secondary"]) != null && $orderCustomer["phone_secondary"] != ""){
+							$phone = $orderCustomer["phone_secondary"];
+						}
+						
+						
+						//Add items of orders to array
+						array_push($order, array(
+							'',
+							$orderId,
+							$orderCustomer["company"],
+							$orderCustomer["firstname"] . " " . $orderCustomer["lastname"],
+							$orderCustomer["street_1"] . " " . $orderCustomer["street_2"],
+							$orderCustomer["zip_code"],
+							$orderCustomer["city"],
+							$orderCustomer["country"],
+							'',
+							$orderShipping["company"],
+							$orderShipping["firstname"] . " " . $orderShipping["lastname"],
+							$orderShipping["street_1"] . " " . $orderShipping["street_2"],
+							$orderShipping["zip_code"],
+							$orderShipping["city"],
+							$orderShipping["country"],
+							'',
+							$ol->getData()["offer"]->getData()["sku"],
+							$orderOffer->getData()["price"],
+							$shippingPrice,
+							$ol->getData()["commission"]->getData()["fee"] / $ol->getData()["quantity"],
+							$orderData["has_customer_message"],
+							$orderData["paymentType"],
+							$ol->getData()["quantity"],
+							$orderHistory->getData()["created_date"]->format('Y-m-d\TH:i:s'),
+							$orderHistory->getData()["last_updated_date"]->format('Y-m-d\TH:i:s'),
+							$orderData["acceptance_decision_date"]->format("Y-m-d\TH:i:s"),
+							date('Y-m-d\TH:i:s', strtotime($last_execution)),
+							$phone
+						));
+					}
+
+					echo $orderId." got parsed. ";
+
+					//Write order to csv
+					writeToCsv($order);
+				
+					//Write current date to txt
+					writeLast();
 				}
 				
-				//Cycle through orderlines
-				foreach($orderLines->getItems() as $ol){					
-					$orderAdditonalFields = $orderData["order_additional_fields"];
-					$orderCustomer = $orderData["customer"]->getData()["billing_address"]->getData();
-					$orderShipping = $orderData["customer"]->getData()["shipping_address"]->getData();		
-					
-					$orderOffer = $ol->getData()["offer"];
-					$orderHistory = $ol->getData()["history"];
-					
-					//Add items of orders to array
-					array_push($order, array(
-						$orderAdditonalFields->getItems()[count($orderAdditonalFields->getItems())-1]->getData()["value"],
-						$orderId,
-						$orderCustomer["company"],
-						$orderCustomer["firstname"] . " " . $orderCustomer["lastname"],
-						$orderCustomer["street_1"] . " " . $orderCustomer["street_2"],
-						$orderCustomer["zip_code"],
-						$orderCustomer["city"],
-						$orderCustomer["country"],
-						'',
-						$orderCustomer["company"],
-						$orderShipping["firstname"] . " " . $orderShipping["lastname"],
-						$orderShipping["street_1"] . " " . $orderShipping["street_2"],
-						$orderShipping["zip_code"],
-						$orderShipping["city"],
-						$orderShipping["country"],
-						'',
-						$ol->getData()["offer"]->getData()["sku"],
-						$orderOffer->getData()["price"],
-						$shippingPrice,
-						$ol->getData()["commission"]->getData()["fee"] / $ol->getData()["quantity"],
-						$orderData["has_customer_message"],
-						$orderData["paymentType"],
-						$ol->getData()["quantity"],
-						$orderHistory->getData()["created_date"]->format('Y-m-d\TH:i:s'),
-						$orderHistory->getData()["last_updated_date"]->format('Y-m-d\TH:i:s'),
-						gmdate("Y-m-d\TH:i:s", time()+3600)						
-					));
-				}
-
-				echo $orderId." got parsed. ";
-
-				//Write order to csv
-				writeToCsv($order);
-			
-				//Write current date to txt
-				writeLast();
 			}
 			
 		}	
@@ -143,45 +173,47 @@ function downloadAPIResult($result, $api_url, $api_key){
 }
 
 /*
-*
+* Generates the headline for the csv file
 */
 function generateHeadline($order){
-		array_push($order, array(
-			'Mail',
-			'Bestellungs-ID',
-			'Rechnungsfirma 1',
-			'Rechnungsfirma 2',
-			'Rechnungsstrasse',
-			'RechnungsPLZ',
-			'Rechnungsort',
-			'Rechnungsland',
-			'Rechnungstelefon',
-			'Versandfirma 1',
-			'Versandfirma 2',
-			'Versandstrasse',
-			'VersandPLZ',
-			'Versandort',
-			'Versandland',
-			'Versandtelefon',
-			'Artikelnummer',
-			'Preis',
-			'Versandkosten',
-			'Nebenkosten',
-			'Notiz',
-			'Bezahlart',
-			'Anzahl bestellt',
-			'Bestellzeitpunkt',
-			'Updatezeitpunkt',
-			'Abholzeitpunkt'
-		));
-		return $order;
+	array_push($order, array(
+		'Mail',
+		'Bestellungs-ID',
+		'Rechnungsfirma 1',
+		'Rechnungsfirma 2',
+		'Rechnungsstrasse',
+		'RechnungsPLZ',
+		'Rechnungsort',
+		'Rechnungsland',
+		'Rechnungstelefon',
+		'Versandfirma 1',
+		'Versandfirma 2',
+		'Versandstrasse',
+		'VersandPLZ',
+		'Versandort',
+		'Versandland',
+		'Versandtelefon',
+		'Artikelnummer',
+		'Preis',
+		'Versandkosten',
+		'Nebenkosten',
+		'Notiz',
+		'Bezahlart',
+		'Anzahl bestellt',
+		'Bestellzeitpunkt',
+		'Updatezeitpunkt',
+		'Bezahlzeitpunkt',
+		'Abholzeitpunkt',
+		'Telefon'
+	));
+	return $order;
 }
 
 /*
 * Write the last execution date to txt.
 */
 function writeLast(){
-	$time = date("Y-m-d\TH:i:s", strtotime('-1 minute'));
+	$time = date("Y-m-d\TH:i:s", strtotime('-10 seconds'));
 	$fp = fopen('last.txt', 'w+');
 	fwrite($fp, $time);
 	fclose($fp);
